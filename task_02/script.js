9b643411c8ec62df53c78dd6aa0290826d887fef
dzimiks <vana997@gmail.com>
2020-03-06 23:56:25 +0100

    Solve #7 issue

---
+++ task_02/script.js
@@ -0,0 +1,12 @@
+const sidebarButton = document.getElementById('sidebar-btn');
+const SIDEBAR_CLASSNAME = 'sidebar';
+const PAGE_CONTENT_CLASSNAME = 'page-content';
+const BUTTON_OPENED_CLASSNAME = 'btn-opened';
+const SIDEBAR_CLOSED_CLASSNAME = 'sidebar-closed';
+const PAGE_CONTENT_ACTIVE_CLASSNAME = 'page-content-active';
+
+sidebarButton.addEventListener('click', () => {
+	sidebarButton.classList.toggle(BUTTON_OPENED_CLASSNAME);
+	document.getElementsByClassName(SIDEBAR_CLASSNAME)[0].classList.toggle(SIDEBAR_CLOSED_CLASSNAME);
+	document.getElementsByClassName(PAGE_CONTENT_CLASSNAME)[0].classList.toggle(PAGE_CONTENT_ACTIVE_CLASSNAME);
+});
