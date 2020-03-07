9fb45a7a326f95c20609ccc29e4af32145489c92
dzimiks <vana997@gmail.com>
2020-03-07 16:16:34 +0100

    Add scroll lock and cross browser compatibility

--- task_02/script.js
+++ task_02/script.js
@@ -1,4 +1,7 @@
 const sidebarButton = document.getElementById('sidebar-btn');
+const body = document.body;
+
+const LOCK_SCROLL_CLASSNAME = 'lock-scroll';
 const SIDEBAR_CLASSNAME = 'sidebar';
 const PAGE_CONTENT_CLASSNAME = 'page-content';
 const BUTTON_OPENED_CLASSNAME = 'btn-opened';
@@ -6,6 +9,7 @@ const SIDEBAR_CLOSED_CLASSNAME = 'sidebar-closed';
 const PAGE_CONTENT_ACTIVE_CLASSNAME = 'page-content-active';
 
 sidebarButton.addEventListener('click', () => {
+	body.classList.toggle(LOCK_SCROLL_CLASSNAME);
 	sidebarButton.classList.toggle(BUTTON_OPENED_CLASSNAME);
 	document.getElementsByClassName(SIDEBAR_CLASSNAME)[0].classList.toggle(SIDEBAR_CLOSED_CLASSNAME);
 	document.getElementsByClassName(PAGE_CONTENT_CLASSNAME)[0].classList.toggle(PAGE_CONTENT_ACTIVE_CLASSNAME);
