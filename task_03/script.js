ed264d2b1649e78228b19bb103d67d9dd35b1efd
dzimiks <vana997@gmail.com>
2020-03-19 17:03:05 +0100

    Add main logic for the game

--- task_03/script.js
+++ task_03/script.js
@@ -1,69 +1,85 @@
-let xTurn = true;
-let gameOver = false;
 let numberOfMoves = 0;
+let gameActive = true;
+let currentPlayer = 'X';
+let gameState = ['', '', '', '', '', '', '', '', ''];
 
-const board = [
-    '', '', '',
-    '', '', '',
-    '', '', '',
-];
+const winningMessage = () => `Player ${currentPlayer} has won in ${numberOfMoves} moves!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+const statusDisplay = document.querySelector('.game--status');
+const gameResult = document.querySelector('.game__result');
+statusDisplay.innerHTML = currentPlayerTurn();
 
-const combinationsToCheck = {
-    0 : [[1, 2], [3, 6], [4, 8]],
-    1 : [[0, 2], [4, 7]],
-    2 : [[0, 1], [5, 8], [4, 6]],
-    3 : [[0, 6], [4, 5]],
-    4 : [[3, 5], [1, 7], [0, 8], [2, 6]],
-    5 : [[3, 4], [2, 8]],
-    6 : [[0, 3], [7, 8], [2, 4]],
-    7 : [[1, 4], [6, 8]],
-    8 : [[6, 7], [2, 5], [0, 4]],
-}
+const COMBINATIONS = {
+	0: [[1, 2], [3, 6], [4, 8]],
+	1: [[0, 2], [4, 7]],
+	2: [[0, 1], [5, 8], [4, 6]],
+	3: [[0, 6], [4, 5]],
+	4: [[3, 5], [1, 7], [0, 8], [2, 6]],
+	5: [[3, 4], [2, 8]],
+	6: [[0, 3], [7, 8], [2, 4]],
+	7: [[1, 4], [6, 8]],
+	8: [[6, 7], [2, 5], [0, 4]],
+};
 
-const boxes = document.querySelectorAll(".board__box");
+const handleCellPlayed = (clickedCell, clickedCellIndex) => {
+	gameState[clickedCellIndex] = currentPlayer;
+	clickedCell.innerHTML = currentPlayer;
+	numberOfMoves++;
+};
 
-boxes.forEach(function(box) {
-    box.addEventListener("click", function() {
-        if (this.innerHTML !== '' || gameOver || numberOfMoves === 9) {
-            return;
-        }
+const handlePlayerChange = () => {
+	currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+	statusDisplay.innerHTML = currentPlayerTurn();
+};
 
-        let currentSymbol = xTurn ? 'X' : 'O';
+const handleResultValidation = (clickedCellIndex) => {
+	let roundWon = false;
+	const combinations = COMBINATIONS[clickedCellIndex];
 
-        box.innerHTML = currentSymbol;
+	combinations.forEach(([first, second]) => {
+		if (gameState[first] === currentPlayer && gameState[second] === currentPlayer) {
+			roundWon = true;
+		}
+	});
 
-        let boxPosition = Number(this.className.split(' ')[1]);
-        
-        board[boxPosition] = currentSymbol;
+	if (roundWon) {
+		statusDisplay.innerHTML = winningMessage();
+		gameResult.innerHTML = winningMessage();
+		gameActive = false;
+		return;
+	}
 
-        numberOfMoves++;
+	const roundDraw = !gameState.includes('');
 
-        checkBoard(boxPosition, currentSymbol);
+	if (roundDraw) {
+		statusDisplay.innerHTML = drawMessage();
+		gameActive = false;
+		return;
+	}
 
-        xTurn = !xTurn;
-    });
-});
+	handlePlayerChange();
+};
 
-function checkBoard(boxPosition, currentSymbol) {
-    const combinations = combinationsToCheck[boxPosition];
-    
-    let won  = false;
+const handleCellClick = (event) => {
+	const clickedCell = event.target;
+	const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
-    combinations.forEach((combination) => {
-        if(board[combination[0]] === currentSymbol && board[combination[1]] === currentSymbol) {
-            won = true;
-            return;
-        }
-    });
+	if (gameState[clickedCellIndex] !== '' || !gameActive || numberOfMoves === 9) {
+		return;
+	}
 
-    if (won) {
-        console.log(currentSymbol + " won in " + numberOfMoves + " moves!");
-        gameOver = true;
-    } 
-    
-    if (numberOfMoves === 9) {
-        console.log("Game over!");
-    }
-}
+	handleCellPlayed(clickedCell, clickedCellIndex);
+	handleResultValidation(clickedCellIndex);
+};
 
+const handleRestartGame = () => {
+	gameActive = true;
+	currentPlayer = 'X';
+	gameState = ['', '', '', '', '', '', '', '', ''];
+	statusDisplay.innerHTML = currentPlayerTurn();
+	document.querySelectorAll('.board__box').forEach((cell) => cell.innerHTML = '');
+};
 
+document.querySelectorAll('.board__box').forEach((cell) => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
