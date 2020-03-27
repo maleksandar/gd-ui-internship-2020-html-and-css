2bc0537bc0d0c5b13ee0a3f134b93369b3426ea3
dzimiks <vana997@gmail.com>
2020-03-27 20:02:48 +0100

    Set navbar to be fixed on top when user scrolls bellow header section

--- task_05/js/script.js
+++ task_05/js/script.js
@@ -1,8 +1,27 @@
 const navbarToggler = document.getElementById('navbar__toggler');
 const navbarNav = document.querySelector('.navbar__nav');
+const navbar = document.querySelector('.navbar');
 
 const NAVBAR_CLOSED_CLASSNAME = 'navbar__nav--closed';
+const NAVBAR_FIXED_CLASSNAME = 'navbar--fixed';
 
+// Toggle navbar
 navbarToggler.addEventListener('click', () => {
 	navbarNav.classList.toggle(NAVBAR_CLOSED_CLASSNAME);
+});
+
+// Set navbar to be fixed on top when user scrolls bellow header section
+// Cross-browser compatibility
+const supportPageOffset = window.pageXOffset !== undefined;
+const isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat');
+const headerHeight = document.querySelector('.header').offsetHeight;
+
+window.addEventListener('scroll', () => {
+	const pageYOffset = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+
+	if (pageYOffset >= headerHeight) {
+		navbar.classList.add(NAVBAR_FIXED_CLASSNAME);
+	} else {
+		navbar.classList.remove(NAVBAR_FIXED_CLASSNAME);
+	}
 });
