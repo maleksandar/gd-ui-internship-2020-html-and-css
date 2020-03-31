0ed7cdc1c9b5c7ed391eece920b3f22135119ec2
vstefanovic-grid <vstefanovic@griddynamics.com>
2020-03-31 21:01:20 +0200

    Add what-we-do secion

--- task_05/script.js
+++ task_05/script.js
@@ -1,6 +1,23 @@
-const hamburgerButton = document.querySelector('body > header > nav > div.navbar__box-essentials > div.navbar__essential-controls > a.fas.fa-bars.btn');
-const controlList = document.querySelector('body > header > nav > div.navbar__other-controls');
+const hamburgerButton = document.querySelector(
+  'body > header > nav > div.navbar__box-essentials > div.navbar__essential-controls > a.fas.fa-bars.btn',
+);
+const controlList = document.querySelector(
+  'body > header > nav > div.navbar__other-controls',
+);
+hamburgerButton.addEventListener('click', () => {
+  controlList.classList.toggle('u-collapsed');
+});
 
-hamburgerButton.addEventListener('click' , () => {
-controlList.classList.toggle('u-collapsed');
+// makes sure only one collapsible with same class can be open
+const collapsibles = Array.from(
+  document.getElementsByClassName('collapsible__toggle'),
+);
+collapsibles.forEach((collapsible) => {
+  collapsible.addEventListener('change', (e) => {
+    collapsibles.forEach((collapsible) => {
+      if (collapsible != e.target) {
+        collapsible.checked = false;
+      }
+    });
+  });
 });
