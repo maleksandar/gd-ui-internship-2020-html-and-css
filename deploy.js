6f07ffc7d8abf31fc8b4c41757ea9d2baa2f841d
Aleksandar Milosavljevic <almi@Aleksandars-MacBook-Pro.local>
2020-04-13 15:34:30 +0200

    Refactoring deployment and building scripts

--- deploy.js
+++ deploy.js
@@ -1,25 +1,31 @@
 var fs = require('fs-extra');
 
+// Must be the same as in Netlify configuration
+const outputDir = 'dist';
 
-fs.removeSync('dist');
-fs.copy('task_00', 'dist/task_00', errorHandler());
-fs.copy('task_01', 'dist/task_01', errorHandler());
-fs.copy('task_02', 'dist/task_02', errorHandler());
-fs.copy('task_03', 'dist/task_03', errorHandler());
-fs.copy('task_04', 'dist/task_04', errorHandler());
-fs.copy('task_05', 'dist/task_05', errorHandler());
-fs.copy('task_06', 'dist/task_06', errorHandler());
-fs.copy('task_07', 'dist/task_07', errorHandler());
-fs.copy('index.html', 'dist/index.html', errorHandler());
-fs.copy('reset.css', 'dist/reset.css', errorHandler());
-fs.copy('style.css', 'dist/style.css', errorHandler());
+const filesToCopy = [
+    'task_00',
+    'task_01',
+    'task_02',
+    'task_03',
+    'task_04',
+    'task_05',
+    'task_06',
+    'task_07',
+    'index.html',
+    'reset.css',
+    'style.css',
+];
 
+filesToCopy.forEach(fileName => {
+    fs.copy(fileName, `${outputDir}/${fileName}`, copyFinnishHandler(fileName, outputDir));
+});
 
-function errorHandler() {
+function copyFinnishHandler(fileName, destination) {
     return function (err) {
         if (err) {
             return console.error(err);
         }
-        console.log('done!');
+        console.log(`Filed copied from '${fileName} to ${destination}/${fileName} successfully.`);
     };
 }
