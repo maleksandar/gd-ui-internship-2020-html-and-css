16947a536894539d3f74fb3340f466ab5d20103c
Aleksandar Milosavljevic <almi@Aleksandars-MacBook-Pro.local>
2020-04-13 12:17:54 +0200

    copy css files

--- deploy.js
+++ deploy.js
@@ -11,6 +11,9 @@ fs.copy('task_05', 'dist/task_05', errorHandler());
 fs.copy('task_06', 'dist/task_06', errorHandler());
 fs.copy('task_07', 'dist/task_07', errorHandler());
 fs.copy('index.html', 'dist/index.html', errorHandler());
+fs.copy('reset.css', 'dist/reset.css', errorHandler());
+fs.copy('style.css', 'dist/style.css', errorHandler());
+
 
 function errorHandler() {
     return function (err) {
