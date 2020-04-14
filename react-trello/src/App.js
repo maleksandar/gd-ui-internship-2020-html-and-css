0374fc2b263b944cfe6bf2c7754afe2a5a05ccc0
crypt096 <acvjetan@gmail.com>
2020-04-14 17:38:29 +0200

    Changed title to test preview.

--- react-trello/src/App.js
+++ react-trello/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi there my name is Alex
         </a>
       </header>
     </div>
