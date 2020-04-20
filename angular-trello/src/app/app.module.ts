30e1801112171f631fbd7a03a9b08fcf7ffe4f50
ivansimic996 <isimic@griddynamics.com>
2020-04-20 17:54:37 +0200

    Initial setup

--- angular-trello/src/app/app.module.ts
+++ angular-trello/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
