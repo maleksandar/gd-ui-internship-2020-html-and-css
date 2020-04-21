e976ecd799e69fca464ac71cac1ec4c16ded05cb
ivansimic996 <isimic@griddynamics.com>
2020-04-21 20:50:23 +0200

    Initial version

--- angular-trello/src/app/app.module.ts
+++ angular-trello/src/app/app.module.ts
@@ -1,20 +1,26 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { TaskListComponent } from './task-list/task-list.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent,
-    TaskListComponent
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-trello/src/app/main/main.component.ts
+++ angular-trello/src/app/main/main.component.ts
@@ -1,4 +1,8 @@
+import { listEntry as Task } from './../models/listEntry.model';
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
 
 @Component({
   selector: 'app-main',
@@ -7,9 +11,123 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  toDo : Array<Task>;
+  inProgress: Array<Task>; 
+  done: Array<Task>;
+
+
+  newTaskModal = false;
+  editTaskModal = false;
+  
+  newTaskTitle= "";
+  newTaskContent= "";
+  test= "test";
+
+  editTaskTitle="";
+  editTaskContent="";
+  editTasklist;
+  editTaskIndex;
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
+       
+    if (localStorage.getItem("toDo")) {
+      this.toDo = JSON.parse(localStorage.getItem("toDo"));
+  
+      } else {
+        this.toDo  = new Array<Task>();
+      }
+
+    if (localStorage.getItem("inProgress")) {
+      this.inProgress = JSON.parse(localStorage.getItem("inProgress"));
+
+      } else {
+        this.inProgress  = new Array<Task>();
+      }  
+
+    if (localStorage.getItem("done")) {
+      this.done = JSON.parse(localStorage.getItem("done"));
+  
+      } else {
+        this.done  = new Array<Task>();
+      }
+  }
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex
+        ); 
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      )
+    
+    }
+
+    this.updateLocalStorage();
+
+  }
+  updateLocalStorage() {
+    localStorage.setItem("toDo", JSON.stringify(this.toDo));
+    localStorage.setItem("inProgress", JSON.stringify(this.inProgress));
+    localStorage.setItem("done", JSON.stringify(this.done));
+  }
+
+  toggleNewTaskModal() {
+    this.newTaskModal = this.newTaskModal ? false: true;
+    this.clearNewTaskText();
+  }
+
+  createNewTask() {
+
+    let newTask = new Task();
+    newTask.title = this.newTaskTitle;
+    newTask.description = this.newTaskContent;
+
+    newTask.descriptionShort = this.shortenDescription(newTask.description);
+    
+    this.toDo.push(newTask);
+    this.toggleNewTaskModal();
+    this.updateLocalStorage();
+
+  }
+
+  clearNewTaskText() {
+    this.newTaskTitle="";
+    this.newTaskContent="";
+  }
+
+  openEditTask(list, index) {
+    this.editTaskTitle = list[index].title;
+    this.editTaskModal = true;
+    this.editTaskContent = list[index].description;
+    this.editTasklist = list;
+    this.editTaskIndex = index;
+  }
+
+  deleteTask(list, index) {
+    list.splice(index, 1);
+    this.updateLocalStorage();
+  }
+
+  saveEditedTask() {
+  
+    this.editTasklist[this.editTaskIndex].title = this.editTaskTitle;
+    this.editTasklist[this.editTaskIndex].description = this.editTaskContent;
+    this.editTasklist[this.editTaskIndex].descriptionShort = this.shortenDescription(this.editTaskContent);
+    this.updateLocalStorage();
+    this.editTaskModal = false;
+  }
+
+  shortenDescription(description) {
+   return  (description.length) < 200 ? description : description.substr(0,200) + "...";
   }
 
 }
