ddc707cceadb598c5dead7fbd4e9493c8a23dede
ivansimic996 <isimic@griddynamics.com>
2020-04-23 21:04:38 +0200

    Initial react setup

---
+++ react-trello/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Header.scss";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <header className="header">
+        <h1>TrelloWannabe</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
